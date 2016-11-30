6af7888b3b3f4b26cf5d79d2ce20f1ebb6dcc564
PorterK <whyisumad@me.com>
2016-11-30 14:50:36 -0500

    Added a few quick notes to self

--- api/upload.js
+++ api/upload.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const _ = require('lodash');
 const router = express.Router();
 
+//all current comments are notes-to-self
+
 router.use(formidable({
   encoding: 'utf-8',
   uploadDir: path.dirname('./i/*')
@@ -13,10 +15,12 @@ router.use(formidable({
 router.post('/', (req, res) => {
   const filePath = req.files.image.path
 
+  //shorten the filenames and move them to different paths soon
   const newfilePath = `${_.replace(filePath, 'upload_', '')}.png`;
 
   fs.rename(filePath, newfilePath);
 
+  //send back the link to the image eventually.
   res.send('Upload');
 });
 
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use('/api', api);
 
+//need to make it so that the directories (whatever they end up being) are served publically.
+//possibly create some sort of authentication so the client can communicate with the API (and other clients can't)
+
 app.listen(3000, (e) => {
   if(e) {
     console.log('Failed to bing port')
